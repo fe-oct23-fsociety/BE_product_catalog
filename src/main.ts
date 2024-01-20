1aa239989718a620e0b0e5f4461e81a082c56d40
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-01-20 15:59:35 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,31 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 
-const PORT = 3001;
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+import { sequelize } from './db.js';
+dotenv.config();
+
+const { DB_HOST, DB_NAME, DB_USER, DB_PORT, DB_PASS } = process.env;
+
+const PORT = process.env.PORT || 3001;
 
 function createServer() {
   const app = express();
 
   app.use(cors());
   app.use(express.json());
+
+  console.log(DB_HOST);
+  
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    });
 
   return app;
 }
