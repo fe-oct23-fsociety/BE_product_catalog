952b0adbf1078dc271c2ee5bd87c06cfa46db779
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-01-21 08:51:33 +0200

    x

---
+++ dist/db.js
@@ -0,0 +1,17 @@
+'use strict';
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+dotenv.config();
+const { DB_URL } = process.env;
+if (!DB_URL) {
+    throw new Error('Something bad with env variables!');
+}
+export const sequelize = new Sequelize(DB_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+});
---
+++ dist/main.js
@@ -0,0 +1,25 @@
+'use strict';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { sequelize } from './db.js';
+dotenv.config();
+const { DB_HOST, DB_NAME, DB_USER, DB_PORT, DB_PASS } = process.env;
+const PORT = process.env.PORT || 3001;
+function createServer() {
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
+    sequelize
+        .authenticate()
+        .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+        .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+    return app;
+}
+createServer().listen(PORT, () => {
+    console.log(`Server is listening PORT ${PORT}`);
+});
