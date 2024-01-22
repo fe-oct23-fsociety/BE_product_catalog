6d540a2f74c5056c83213169e0546d637eef8999
Nazar Boblyk <fs.boblyk.nazar@gmail.com>
2024-01-22 22:15:26 +0200

    added migration Products, phones, tablets, accessories

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ import dotenv from 'dotenv';
 import { sequelize } from './db.js';
 dotenv.config();
 
-// const { DB_HOST, DB_NAME, DB_USER, DB_PORT, DB_PASS } = process.env;
-
 const PORT = process.env.PORT ?? 3001;
 
 function createServer (): Express {
