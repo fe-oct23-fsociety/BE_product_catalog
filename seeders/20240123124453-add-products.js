a368d77296679bae42954a6281245e944a3b2acc
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-01-23 17:13:46 +0200

    fixed cjs extensions(3)

--- seeders/20240123124453-add-products.js
+++ seeders/20240123124453-add-products.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const products = require('./20240123124453-add-products.json');
+import products from './20240123124453-add-products.json';
 
-module.exports = {
-  async up (queryInterface) {
+export default {
+  async up(queryInterface) {
     await queryInterface.bulkInsert(
       'Products',
       products,
-    )
+    );
   },
 
-  async down (queryInterface) {
+  async down(queryInterface) {
     await queryInterface.bulkDelete('Products', null, {});
   }
 };
--- seeders/20240123124524-add-products-details.js
+++ seeders/20240123124524-add-products-details.js
@@ -2,15 +2,15 @@
 
 const products_details = require('./20240123124524-add-products-details.json');
 
-module.exports = {
-  async up (queryInterface) {
+export default {
+  async up(queryInterface) {
     await queryInterface.bulkInsert(
       'Products-details',
       products_details,
     )
   },
 
-  async down (queryInterface) {
+  async down(queryInterface) {
     await queryInterface.bulkDelete('Products-details', null, {});
   }
 };
