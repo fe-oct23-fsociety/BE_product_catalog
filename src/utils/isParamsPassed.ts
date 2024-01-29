d7f7a44e78f4a0c1c08e9c3e17b56be09a7becba
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-01-29 20:08:28 +0200

    small fix

--- src/utils/isParamsPassed.ts
+++ src/utils/isParamsPassed.ts
@@ -1,7 +1,7 @@
 import { isString } from './isString.js';
 
 export const isParamsPassed = (param: any): number | undefined => {
-  if(isString(param)) {
+  if (isString(param)) {
     return Number(param);
   }
   return undefined;
